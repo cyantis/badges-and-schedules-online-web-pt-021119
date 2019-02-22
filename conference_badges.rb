e3e54c2ec2beae050cbf5dd161f226e1c083e386
Cody Yantis <cyantis@gmail.com>
2019-02-22 01:48:29 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index.collect {|name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+  attendees.collect.with_index(1) {|name, index| "Hello, #{name}! You'll be assigned to room #{index}!"}
 end
 
 def printer(attendees)
